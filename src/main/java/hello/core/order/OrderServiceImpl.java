a268c039b6c7564360984f467b8cafd51d5220c7
alpacaHyun <flaehdan@gmail.com>
2023-01-01 15:06:23 +0900

    feat : 새로운 가격정책
    body :

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // OrderServiceImpl 은 DiscountPolicy 와 FixDiscountPolicy 모두에 의존하고 있다. 즉 추상과 구체 모두에게 의존하고 있다.
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+    // 주문을 해서 반환해주는 역할을 담당
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        Long memberId = 1L;  // primitive type 인 long 으로 하면 null 을 넣을 수 없기 때문에 Wrapper 타입인 Long 사용
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
